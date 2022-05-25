c5edb70f7da32e12e8382f3712da2326d541d1fc
kymap057 <vohoangky147@gmail.com>
2022-05-25 22:55:36 +0700

    Update UI and add json info

---
+++ info.js
@@ -0,0 +1,77 @@
+const INFO = {
+    Name: "VO HOANG KY",
+    major: "WEB DEVELOPER",
+    contacts: {
+        birthday: "16/02/1999",
+        address: "TP.HCM",
+        phone: {
+            link: "tel:038817767",
+            display: "(+84) 388 417 767"
+        },
+        faceBook: {
+            link: "https://www.facebook.com/ky.vhk",
+            display: "Hoàng Kỳ"
+        },
+        mail: {
+            link: "mailto:vohoangky147@gmail.com",
+            display: "vohoangky147@gmail.com"
+        },
+        github: {
+            link: "https://github.com/kymap057",
+            display: "github.com/kymap057"
+        }
+    },
+    summary: `My name's Vo Hoang Ky, I am passionate about technology and ready to learn. I want to participate in team working that discuss programming or technological issues and contribute ideas, share experiences during group discussions or learning. Besides, I want to have a job and a working environment friendly and sociable. Sometimes I play sports and attend extracurricular events. I hope to participate in development in projects, deployed to multiple users and programming skills are improving.`,
+    education: [
+        {
+            school: "LE THANH PHUONG HIGH SCHOOL.",
+            time: "AUGUST 2014 - MAY 2017",
+            status: "Completed high school education."
+        },
+        {
+            school: "SAIGON TECHNOLOGY UNIVERSITY.",
+            time: "AUGUST 2017 - AUGUST 2021",
+            status: "Graduated august 2021 with good grades."
+        }
+    ],
+    experience: [
+        {
+            company: "My project",
+            body: [
+                {
+                    name: "Student management software project.",
+                    time: "November 2019",
+                    description: "Building software helps to knowledge OOP, and understanding of database constraints in mySQL.",
+                    position: "Dev.",
+                    language: "c#, mysql"
+                },
+                {
+                    name: "PROJECT NEWS WEB",
+                    time: "AUGUST 2020",
+                    description: "Build a utility page that provides quick news and weather lookups using data from APIs. Frontend built by ejs, css, javascript. backend using programming language nodejs and express framework.",
+                    position: "Dev",
+                    language: "ejs, css, javascript, nodejs"
+                },
+                {
+                    name: "PROJECT WEB SHOP CLOCK",
+                    time: "November 2020",
+                    description: "Build an e-commerce site with database connection mySQL. Site interface built by html, css, javascript. Backend using programming language PHP. And technologies Jquery, Ajax, bootstrap 3.",
+                    position: "Dev",
+                    language: "html, css, php, mysql, javascript"
+                },
+            ]
+        }
+    ],
+    skill: {
+        programmingLanguage: "Javascript, PHP, C++, C#, bash script",
+        devpos: "AWS service, heroku, nginx (nodejs)",
+        sourceManager: "Git, SVN",
+        frontendTech: "HTML5, CSS, SCSS, Javascript, Bootstrap , EJS, ReactJS",
+        backendTech: "Nodejs, PHP, YAML",
+        database: "MySQL, PostgreSQL, Mongodb.",
+        IDE: "Visual Code, Visual Studio, Sublime text 3, Postman, Git",
+        frameworks: "Express, jQuery, ReactJS, Bootstrap.",
+        language: "English, vietnamese."
+    }
+
+}
